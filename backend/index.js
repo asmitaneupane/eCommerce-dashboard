7546d16ddcd90fe868b9c80f95111ca254da2487
asmitaneupane <neupane.ashmita2056@gmail.com>
2023-03-03 13:21:23 +0545

    update and delete products

--- backend/index.js
+++ backend/index.js
@@ -47,4 +47,40 @@ app.get('/products', async (req, res) => {
     }
 })
 
+app.delete('/product/:id', async (req, res) => {
+    let result = await Product.deleteOne({ _id: req.params.id });
+    res.send(result);
+})
+
+app.get('/product/:id', async (req, res) => {
+    let result = await Product.findOne({ _id: req.params.id });
+    if (result) {
+        res.send(result)
+    } else {
+        res.send({ result: "No Record Match." })
+    }
+})
+
+app.put('/product/:id', async (req, res) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        {
+            $set: req.body
+        }
+    );
+    res.send(result);
+})
+
+app.get('/search/:key', async (req, res) => {
+    let result = await Product.find({
+        "$or": [
+            { name: { $regex: req.params.key } },
+            { price: { $regex: req.params.key } },
+            { category: { $regex: req.params.key } },
+            { company: { $regex: req.params.key } },
+        ]
+    });
+    res.send(result);
+})
+
 app.listen(5001)
